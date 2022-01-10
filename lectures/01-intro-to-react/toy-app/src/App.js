88cf16b1efce367b548ee251d255ac86900604ea
Louis Michael Medina <louis@Louis-Medinas-MacBook-Pro.local>
2022-01-10 17:44:08 -0500

    add solutions for 01-intro-to-react

--- lectures/01-intro-to-react/toy-app/src/App.js
+++ lectures/01-intro-to-react/toy-app/src/App.js
@@ -1,64 +1,95 @@
 /* 
-    - Break Out Activity #1: Convert HTML to JSX
+    - Break Out Activity #1: Take HTML from Toy Tales lab (Lines 9 - 48) and turn it into JSX
+        - https://github.com/learn-co-curriculum/phase-1-challenge-toy-tale/blob/master/index.html
         - What needs to change? What's the same?
-        - Using comments, begin to map out JSX elements into into separate components
+        - Refactor HTML into separate components
 
     - Break Out Activity #2: Implement a ToyCard Component
         - Start by hard-coding the JSX in your App component return statement
         - Use props to make your component reusable
         - Copy/paste data from the "toys.js" file as props and iterate over Toy objects
-          to create a ToyCard component for each one
-        - BONUS: 
-          - Import and store data from the "toys.js" file
-          - Create a "components" subdirectory + "ToyCard.js" file
+          to create a ToyCard component for each
+        - BONUS: Try importing and storing data from the "toys.js" file
 */
 
+// Break Out Activity #2: Import Toys Array from toys.js
+import MyToys from './toys';
+
+// Break Out Activity #2: Import Custom Components
+import ToyCard from './components/ToyCard';
+
+// Root App Component Function
 function App() {
-  return (
-    <div>
-      <header className="App-header">
-      <div id="toy-header">
-      <img
-        src="https://fontmeme.com/permalink/180719/67429e6afec53d21d64643101c43f029.png"
-        alt="toy-header"
-      />
-    </div>
+    return (
+        // Must return JSX as one Parent Level Element
+        <div>
+            {/* Break Out Activity #1: Refactor HTML into JSX */}
+            
+            {/* Header Component */}
+            <div id="toy-header">
+                <img
+                    src="https://fontmeme.com/permalink/180719/67429e6afec53d21d64643101c43f029.png"
+                    alt="toy-header"
+                />
+            </div>
 
-    <div class="container">
-      <form class="add-toy-form">
-        <h3>Create a toy!</h3>
+            {/* Container Component */}
+            <div className="container">
+                
+                {/* Form Component */}
+                <form className="add-toy-form">
+                    <h3>Create a toy!</h3>
 
-        <input
-          type="text"
-          name="name"
-          value=""
-          placeholder="Enter a toy's name..."
-          class="input-text"
-        />
-        <br />
-        <input
-          type="text"
-          name="image"
-          value=""
-          placeholder="Enter a toy's image URL..."
-          class="input-text"
-        />
-        <br />
-        <input
-          type="submit"
-          name="submit"
-          value="Create Toy"
-          class="submit"
-        />
-      </form>
-    </div>
-    <p style="text-align:center">
-      Andy needs your help! <button id="new-toy-btn">Add a new toy!</button>
-    </p>
+                    {/* Input Component */}
+                    <input
+                        type="text"
+                        name="name"
+                        // value=""
+                        placeholder="Enter a toy's name..."
+                        className="input-text"
+                    />
+                    <br />
 
-    <div id="toy-collection"></div>
-      </header>
-    </div>
+                    {/* Input Component */}
+                    <input
+                        type="text"
+                        name="image"
+                        // value=""
+                        placeholder="Enter a toy's image URL..."
+                        className="input-text"
+                    />
+                    <br />
+
+                    {/* Input Component */}
+                    <input
+                        type="submit"
+                        name="submit"
+                        value="Create Toy"
+                        className="submit"
+                    />
+                </form>
+            </div>
+            
+            {/* Hero Component */}
+            
+            <p style={{textAlign: 'center'}}>
+                Andy needs your help! <button id="new-toy-btn">Add a new toy!</button>
+            </p>
+
+            {/* ToyCollection Component */}
+            <div id="toy-collection">
+        
+                {/* Map over Array of Toy Objects */}
+                {MyToys.map(toy => {
+                
+                    // For each Toy object, create a ToyCard Component and pass
+                    // the Toy object as a prop, "toy"
+                    return (
+                        <ToyCard key={toy.id} toy={toy}/>
+                    );
+                })}
+            </div>
+        </div>
   );
 }
 
--- lectures/01-intro-to-react/toy-app/src/index.js
+++ lectures/01-intro-to-react/toy-app/src/index.js
@@ -1,8 +1,15 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import "./styles.css";
-import App from './App';
+
+import App from "./App";
+
+// ReactDOM.render(
+//      WHAT TO RENDER,
+//      WHERE TO RENDER IT
+// )
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <App />, 
+    document.getElementById("root")
+    // document.querySelector('#root')
 );
