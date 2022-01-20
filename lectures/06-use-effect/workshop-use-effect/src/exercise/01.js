d6b3ff6084c6fc4809b028e67dd730a2fd53d2c4
Louis Michael Medina <louis@Louis-Medinas-MacBook-Pro.local>
2022-01-20 13:31:34 -0500

    add starter code for 07-advanced-http-requests

--- lectures/06-use-effect/workshop-use-effect/src/exercise/01.js
+++ lectures/06-use-effect/workshop-use-effect/src/exercise/01.js
@@ -1,34 +1,41 @@
 // ✅ on line 2, import the useEffect hook along with the useState hook
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [pics, setPics] = useState([]);
+  const [status, setStatus] = useState("pending");
 
   // ✅ call the useEffect hook with a callback function and an empty array as a second argument
-  // 👀 useEffect(() => {}, [])
+  
+  useEffect(() => {
+    console.log("Fetching data...");
+    fetch("https://dog.ceo/api/breeds/image/random/8")
+    .then(res => res.json())
+    .then(data => {
+      console.log("Data fetched!", data);
+      // data is an object with a key of message, which has an array of images
+      // data.message => Array
+      setPics(data.message);
+      // setStatus("success");
+    });
+  }, []);
+
   // ✅ take all the code out from the handleFetchClick function, and put it inside the callback function in your useEffect
   // 📃 Check out the docs for an example: https://reactjs.org/docs/faq-ajax.html#example-using-ajax-results-to-set-local-state
 
   // 🗑 once your useEffect hook works, delete this function and also delete the <button> from the JSX
-  function handleFetchClick() {
-    console.log("Fetching data...");
-    fetch("https://dog.ceo/api/breeds/image/random/8")
-      .then(res => res.json())
-      .then(data => {
-        console.log("Data fetched!", data);
-        // data is an object with a key of message, which has an array of images
-        // data.message => Array
-        setPics(data.message);
-      });
-  }
 
   console.log("Component rendering");
 
+  if (pics.length === 0) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <div style={{ padding: "16px" }}>
+      
       <h1>Dog Pics</h1>
       <DogPics pics={pics} />
-      <button onClick={handleFetchClick}>Fetch!</button>
     </div>
   );
 }
--- lectures/06-use-effect/workshop-use-effect/src/exercise/02.js
+++ lectures/06-use-effect/workshop-use-effect/src/exercise/02.js
@@ -2,13 +2,23 @@ import { useState, useEffect } from "react";
 
 function App() {
   // ✅ update the initial state for text to read a value using localStorage.getItem("text")
-  const [text, setText] = useState("");
-  const [count, setCount] = useState(0);
+  const [text, setText] = useState(localStorage.getItem("text") || "");
+  const [count, setCount] = useState(parseInt(localStorage.getItem("count")) || 0);
+
+  console.log(text);
 
   useEffect(() => {
     // ✅ when the side effect runs, use localStorage.setItem("text", value) to save the text to localStorage
+    
+    // { text: "" }
+    // { text: "a" }
+    // { text: "ab" }
+    // { text: "abc" }
+    // { text: "abcd" }
+    localStorage.setItem("text", text);
+    localStorage.setItem("count", count);
     console.log("Running side effect");
-  });
+  }, [text, count]);
   // ✅ use the dependencies array so that the side effect only runs if the text changes
 
   console.log("Rendering component");
